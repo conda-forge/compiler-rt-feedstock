573fc61b629f1fd0f5f191aee8e89bbbc4c9b008
Isuru Fernando <isuruf@gmail.com>
2019-04-22 00:40:13 -0500

    Build only x86_64 on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@ if [[ "$target_platform" == "osx-64" ]]; then
     export CONDA_BUILD_SYSROOT_BACKUP=${CONDA_BUILD_SYSROOT}
     conda install -p $BUILD_PREFIX --quiet --yes clangxx_osx-64=${cxx_compiler_version}
     export CONDA_BUILD_SYSROOT=${CONDA_BUILD_SYSROOT_BACKUP}
+    EXTRA_CMAKE_ARGS="-DDARWIN_osx_ARCHS=x86_64"
 fi
 
 # Prep build
@@ -20,6 +21,7 @@ cmake \
     -DCMAKE_MODULE_PATH:PATH="${PREFIX}/lib/cmake" \
     -DLLVM_CONFIG_PATH:PATH="${PREFIX}/bin/llvm-config" \
     -DPYTHON_EXECUTABLE:PATH="${BUILD_PREFIX}/bin/python" \
+    ${EXTRA_CMAKE_ARGS} \
     "${SRC_DIR}"
 
 # Build step

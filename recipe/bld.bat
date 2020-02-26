fa401975064f69acb32ea671b02b5d281671aaa6
Isuru Fernando <isuruf@gmail.com>
2020-02-26 06:11:14 +0000

    Fix paths on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,13 +10,13 @@ if errorlevel 1 exit 1
 set "CC=cl.exe"
 set "CXX=cl.exe"
 
+set "INSTALL_PREFIX=%LIBRARY_PREFIX%\lib\clang\%PKG_VERSION%"
+
 cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE="Release" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH="%LIBRARY_BIN%" ^
-    -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH="%LIBRARY_LIB%" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%INSTALL_PREFIX%" ^
     -DCMAKE_MODULE_PATH:PATH="%LIBRARY_LIB%\cmake" ^
     -DLLVM_CONFIG_PATH:PATH="%LIBRARY_BIN%\llvm-config.exe" ^
     "%SRC_DIR%"
@@ -30,3 +30,5 @@ if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 
+mkdir %PREFIX%\lib\clang\%PKG_VERSION%\lib\windows
+copy %INSTALL_PREFIX%\lib\windows\* %PREFIX%\lib\clang\%PKG_VERSION%\lib\windows\

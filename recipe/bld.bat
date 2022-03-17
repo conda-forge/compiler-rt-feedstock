4d25b6880ccb90ec908945cb21261664f5051daf
H. Vetinari <h.vetinari@gmx.com>
2022-03-17 21:22:20 +1100

    %ERROR_LEVEL% beautification

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,17 +5,11 @@ xcopy llvm-project\cmake\Modules\* cmake\modules\
 if %ERRORLEVEL% neq 0 exit 1
 
 mkdir build
-if errorlevel 1 exit 1
-
 cd build
-if errorlevel 1 exit 1
 
 set BUILD_CONFIG=Release
-if errorlevel 1 exit 1
-
 set "CC=clang-cl.exe"
 set "CXX=clang-cl.exe"
-
 set "INSTALL_PREFIX=%LIBRARY_PREFIX%\lib\clang\%PKG_VERSION%"
 
 cmake ^
@@ -26,15 +20,15 @@ cmake ^
     -DCMAKE_MODULE_PATH:PATH="%LIBRARY_LIB%\cmake" ^
     -DLLVM_CONFIG_PATH:PATH="%LIBRARY_BIN%\llvm-config.exe" ^
     "%SRC_DIR%"
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 :: Build step
 nmake
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 :: Install step
 nmake install
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 mkdir %PREFIX%\lib\clang\%PKG_VERSION%\lib\windows
 copy %INSTALL_PREFIX%\lib\windows\* %PREFIX%\lib\clang\%PKG_VERSION%\lib\windows\
